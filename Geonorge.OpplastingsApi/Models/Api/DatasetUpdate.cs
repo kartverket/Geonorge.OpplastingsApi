ca6bd467665a4c3de0c948158fb1583c53c4fab1
Dag Olav Dahle <dagolav@arkitektum.no>
2023-06-22 15:08:57 +0200

    Add required

--- Geonorge.OpplastingsApi/Models/Api/DatasetUpdate.cs
+++ Geonorge.OpplastingsApi/Models/Api/DatasetUpdate.cs
@@ -1,14 +1,21 @@
-﻿namespace Geonorge.OpplastingsApi.Models.Api
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Geonorge.OpplastingsApi.Models.Api
 {
     public class DatasetUpdate
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
-
+        [Required]
         public string MetadataUuid { get; set; }
+        [Required]
         public string ContactEmail { get; set; }
+        [Required]
         public string ContactName { get; set; }
+        [Required]
         public string OwnerOrganization { get; set; }
+        [Required]
         public string RequiredRole { get; set; }
     }
 }
